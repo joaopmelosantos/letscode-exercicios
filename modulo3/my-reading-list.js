64e06c7839e36aa1f2f843c36d5d7d761c5de90e
João Paulo <92185145+joaopmelosantos@users.noreply.github.com>
2021-10-19 19:49:12 -0300

    Create my-reading-list.js

---
+++ modulo3/my-reading-list.js
@@ -0,0 +1,74 @@
+class BookList {
+  constructor() {
+    ;(this.numReadBooks = 0),
+      (this.numUnreadBooks = 0),
+      (this.currentBook = null),
+      (this.lastBookReaded = null),
+      (this.nextBook = null),
+      (this.allBooks = [])
+  }
+
+  addBook(book) {
+    if(this.allBooks.includes(book.title)){
+      return 'Este livro já está na lista'
+    } else {
+      this.allBooks = [...this.allBooks, book]
+      console.log(`O Livro ${book.title} foi adicionado com sucesso!`)
+      this.updateBookList()
+    }
+  }
+
+  finishCurrentBook(){
+    if(this.currentBook != null){
+    this.currentBook.read = true
+    this.currentBook.readDate = new Date()
+    console.log(`Livro ${this.currentBook.title} Finalizado com Sucesso!`)
+    this.updateBookList()
+    } else {return 'Não há livro atual para finalizar'}
+  }
+
+  updateBookList(){
+    this.countBooks()
+    this.orderBookList()
+    this.messageToUsers()
+  }
+
+  countBooks(){
+    this.numReadBooks = this.allBooks.reduce((acc,current)=>current.read ? acc+=1 : acc, 0)
+    this.numUnreadBooks = this.allBooks.length - this.numReadBooks
+    console.log(`Há ${this.numReadBooks} livro(s) lidos e ${this.numUnreadBooks} livro(s) não lidos na lista`)
+  }
+
+  messageToUsers(){
+    console.log('Livro que está lendo: ' + (typeof this.currentBook === 'object' ? this.currentBook.title : 'Você não está lendo livros atualmente'))
+    console.log('Último Livro lido: ' + (typeof this.lastBookReaded === 'object' ? this.lastBookReaded.title : 'Você ainda não terminou de ler livros'))
+    console.log('Próximo Livro: ' + (typeof this.nextBook === 'object' ? this.nextBook.title : 'Você ainda não tem próximos livros para ler'))
+  }
+
+  orderBookList(){
+    const readBookList = this.allBooks.filter(book=>book.read)
+    const unreadBookList = this.allBooks.filter(book=>!book.read)
+    this.currentBook = unreadBookList.length > 0 ? unreadBookList[0] : 'Você não está lendo livros atualmente'
+    this.lastBookReaded = readBookList.length > 0 ? readBookList.reduce(
+      (acc,current)=> current.readDate >= acc.readDate ? current : acc, readBookList[0]) : 'Não há último livro lido'
+    this.nextBook = unreadBookList.length > 1 ? unreadBookList[1] : 'Não há próximo livro'
+  }
+}
+
+class Book {
+  constructor(title, genre, author, read, readDate) {
+    ;(this.title = title),
+      (this.genre = genre),
+      (this.author = author),
+      (this.read = read),
+      (this.readDate = this.read ? this.setDate(readDate) : 'Não finalizado')
+  }
+  setDate(date){
+    if(date === undefined){
+      console.log(`Você não colocou uma data no livro "${this.title}", então colocamos uma para você!`)
+      return this.readDate = new Date()
+    } else {
+      return this.readDate = new Date(date)
+    }
+  }
+}
